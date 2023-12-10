23d23e3a497764be1475291c41284451465e2057
Kaarthik-07 <57kaarthikj@gmail.com>
2023-12-10 20:17:12 +0530

    accerdian frontend commits

---
+++ src/app/login/page.js
@@ -0,0 +1,15 @@
+'use client'
+import SignUpForm from "../components/SignUpForm";
+import LoginForm from "../components/SignUpForm";
+
+
+const Page = () =>{
+    return(
+        <>
+        <SignUpForm />
+        
+        </>
+    )
+}
+
+export default Page;
---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+
+'use client'
+import Image from 'next/image'
+import AuthContainer from './components/AutoContainer'
+import LoginForm from './components/LoginForm'
+
+export default function Home() {
+  return (
+    <>
+    <h1>HI</h1>
+    </>
+
+   
+  )
+}
